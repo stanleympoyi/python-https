f9c99f04a72e64626e264bfe98d2ef97132b7cfc
Stanley Mpoyi <mpoyis@oregonstate.edu>
2022-02-17 11:03:09 -0700

    first get request command

--- https.py
+++ https.py
@@ -1,2 +1,8 @@
-def https():
-  pass
+import requests
+
+
+url = "https://news.ycombinator.com/"
+response = requests.get(url)
+
+print(
+    f"your request to {url} came back with status code {response.status_code}")
